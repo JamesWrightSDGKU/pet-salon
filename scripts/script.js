73a78afb56cb73f32686422cbfe3e5372a0ad4bd
JamesWrightSDGKU <James.Wright@sdgku.edu>
2025-02-05 16:47:43 -0800

    assignment 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,84 @@
-// Assignment 1
-
-let pets = [
-    {
-        name: "Fetcher",
-        age: 6,
-        gender: "male",
-        service: false,
-        breed: "Golden Retriever"
-    },
-    {
-        name: "Laika",
-        age: 4,
-        gender: "female",
-        service: true,
-        breed: "Akita"
-    },
-    {
-        name: "Satchmo",
-        age: 8,
-        gender: "male",
-        service: false,
-        breed: "Saint Bernard"
-    }
-]
-
-function numPets(){
-    document.getElementById("petCounter").innerHTML=pets.length;
-}
-
-numPets();
-
-function petNames(){
-    for(i = 0; i < pets.length; i++){
-        document.getElementById("petNames").innerHTML+=`<li>${pets[i].name}</li>`;
+let petList = [];
+let salon = {
+    name: "The Fashionable Pet",
+    address:{
+        streetNumber:"538",
+        street: "Serene Way",
+        city: "Tropic Bay",
+        state: "CA",
+        zip: "92810",
+        country: "United States of America"
     }
 }
 
-petNames();
+// function getSalonAddress(){
+//     document.getElementById("salonAddress").innerHTML=`<p>${salon.address.streetNumber} ${salon.address.street}</p><p>${salon.address.city}, ${salon.address.state} ${salon.address.zip}</p>`;
+//     // console.log(salon.address.streetNumber);
+// }
+
+// getSalonAddress();
+// getting HTML elements
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputService = document.getElementById("txtService");
+let inputBreed = document.getElementById("txtBreed");
+let inputSpecies = document.getElementById("txtSpecies");
+
+function Pet(name,age,gender,service,breed,species){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.service = service;
+    this.breed = breed;
+    this.species = species;
+}
+
+function register(){
+    // get the values from inputs
+    
+    // create obj using constructor
+    let newPet = new Pet(inputName.value, inputAge.value, inputGender.value, inputService.value, inputBreed.value, inputSpecies.value);
+
+    console.log(newPet);
+
+    // push obj into array
+    petList.push(newPet);
+    clearInputs();
+}
+
+function clearInputs(){
+    inputName.value = "";
+    inputAge.value = "";
+    inputGender.value="";
+    inputService.value="";
+    inputBreed.value="";
+    inputSpecies.value="";
+}
+
+
+function init(){
+    // create three pets
+    let pet1 = new Pet("Fetcher",6,"male","de-shed","Golden Retriever","dog");
+    let pet2 = new Pet("Laika",4,"female","shampoo","Akita","dog");
+    let pet3 = new Pet("Satchmo",8,"male","nail trim","Saint Bernard","dog");
+    
+    // push the pets into the array
+    petList.push(pet1,pet2,pet3);
+    
+    function petCounter(){
+        document.getElementById("petsCounter").innerHTML=`There are ${petList.length} pets registered:`;
+        console.log(petList.length);
+    }
+    petCounter();
+    
+    function petNames(){
+        for(i=0;i<petList.length;i++){
+            document.getElementById("petNames").innerHTML+=`<li>${petList[i].name}</li>`;
+        }
+    }
+    petNames();
+}
+
+
+window.onload=init;// wait to render HTML
---
+++ scripts/session-2.js
@@ -0,0 +1,14 @@
+function Student(name,email,age){
+    this.name = name;
+    this.email = email;
+    this.age = age;
+}
+
+// creating the object
+
+// let objName = new  Constructor("value1","value2","value3");
+let student1 = new Student("Leo","lmiranda@sdgku.edu",35);
+let sutdent2 = new Student("Bruce","bruce@example.com",32);
+let student3 = new Student("Pam","pam@example.com","28");
+
+console.log(student1);
